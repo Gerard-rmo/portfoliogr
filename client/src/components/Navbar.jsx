b17ee78d4a178f90eb79e3dcb4d67433c8a80bdb
Ger-ard <gerard.romero.glaive@gmail.com>
2025-05-06 16:51:22 +0200

    site complet

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import './Navbar.css'; // import du fichier CSS
 
 const Navbar = () => {
   const [isAuth, setIsAuth] = useState(false);
@@ -11,57 +12,23 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav style={styles.nav} role="navigation">
-      <ul style={styles.navList}>
-        <li style={styles.navItem}>
-          <Link to="/" style={styles.btnLink}>Accueil</Link>
+    <nav className="nav" role="navigation">
+      <ul className="navList">
+        <li className="navItem">
+          <Link to="/" className="btnLink">Accueil</Link>
         </li>
-        <li style={styles.navItem}>
-          <Link to="/albums" style={styles.navLink}>Bédéthèque</Link>
+        <li className="navItem">
+          <Link to="/albums" className="navLink">Bédéthèque</Link>
         </li>
-        <li style={styles.navItem}>
-          <Link to="/skates" style={styles.navLink}>Planches de skate</Link>
+        <li className="navItem">
+          <Link to="/skates" className="navLink">Planches de skate</Link>
         </li>
-        <li style={styles.navItem}>
-          <Link to="/salons" style={styles.navLink}>Salons</Link>
+        <li className="navItem">
+          <Link to="/salons" className="navLink">Salons</Link>
         </li>
       </ul>
     </nav>
   );
-};
-
-const styles = {
-  nav: {
-    backgroundColor: 'rgb(28, 91, 143)',
-    padding: '13px',
-  },
-  navList: {
-    listStyleType: 'none',
-    display: 'flex',
-    justifyContent: 'space-around',
-    margin: 0,
-    padding: 0,
-  },
-  navItem: {
-    margin: '0 10px',
-    fontFamily: "'Verdana', serif",
-  },
-  navLink: {
-    color: 'white',
-    textDecoration: 'none',
-    fontSize: '15px',
-  },
-  btnLink: {
-    padding: '5px 10px',
-    backgroundColor: 'rgb(4, 2, 105)',
-    color: 'yellow',
-    borderRadius: '50px',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    boxShadow: '0px 0px 20px rgba(21, 13, 63, 0.93)',
-  },
-  
-  
 };
 
 export default Navbar;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,11 +8,13 @@ import slide4 from '../assets/T3M.jpg';
 import slide5 from '../assets/AKELARRE.jpg';
 import slide6 from '../assets/ALEM.jpg';
 import slide7 from '../assets/MOSQUITO.jpg';
-import slide8 from '../assets/couv - Copie.png';
-import slide9 from '../assets/couv.png';
+import slide8 from '../assets/protecteurs.png';
+import slide9 from '../assets/couv - Copie.png';
+import slide10 from '../assets/couv.png';
+import slide11 from '../assets/az.png';
 import "./HomePage.css";
 
-const slides = [slide1, slide2, slide3, slide4, slide5, slide6,slide7, slide8, slide9]; 
+const slides = [slide1, slide2, slide3, slide4, slide5, slide6,slide7, slide8, slide9, slide10, slide11]; 
 
 const HomePage = () => {
   return (
